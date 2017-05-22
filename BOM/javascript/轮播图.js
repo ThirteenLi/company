ff4ee53fc5206e7999e749d6d9458c14a4b28303
ThriteenLi <a1363252276@126.com>
2017-05-22 22:45:18 +0800

    5.22

--- BOM/javascript/轮播图.js
+++ BOM/javascript/轮播图.js
@@ -12,6 +12,30 @@
     var oContainer=document.getElementById("container");
     var nowindex=1;
     var timer;
+    var oNav=document.getElementById("nav");
+    var aRli=oNav.getElementsByTagName("li");
+    var aBody=document.getElementsByTagName("body")[0];
+    function changeimg(n){
+        for(var i=0;i<aimg.length;i++){
+            aimg[i].className="";
+        }
+        for(var i=0;i<aLi.length;i++){
+            aLi[i].className="";
+        }
+        aimg[n].className="selected";
+        aLi[n].className="selected";
+        aLi[n+1].className="selected sco";
+        aLi[n+2].className="selected";
+    }
+    function play(n){
+        timer=setInterval(function(){
+            oRight.onclick();
+        },n);
+    }
+    function stop(){
+        clearInterval(timer);
+    }
+    play(1000);
     oLeft.onclick=oRight.onclick=function(){
         if(this==oLeft){
             nowindex=(nowindex+22)%23;
@@ -20,6 +44,12 @@
             nowindex=(nowindex+1)%23;
         }
         changeimg(nowindex);
+    };
+    oContainer.onmouseover=function(){
+        stop();
+    };
+    oContainer.onmouseout=function(){
+        play(1000);
     };
     for(var i=0;i<aLi.length;i++){
         aLi[i].index=i-1;
@@ -34,31 +64,11 @@
             changeimg(nowindex);
         };
     }
-    function changeimg(n){
-        for(var i=0;i<aimg.length;i++){
-            aimg[i].className="";
-        }
-        for(var i=0;i<aLi.length;i++){
-            aLi[i].className="";
-        }
-        aimg[n].className="selected";
-        aLi[n].className="selected";
-        aLi[n+1].className="selected sco";
-        aLi[n+2].className="selected";
-    }
-    function play(){
-        timer=setInterval(function(){
-            oRight.onclick();
-        },800);
-    }
-    function stop(){
-        clearInterval(timer);
-    }
-    play();
-    oContainer.onmouseover=function(){
-        stop();
-    };
-    oContainer.onmouseout=function(){
-        play();
+    document.oncontextmenu=function(e){
+        e=e||window.event;
+        oNav.style.display="block";
+        oNav.style.left= (e.clientX<(aBody.clientWidth-102)? e.clientX:(aBody.clientWidth-102))+"px";
+        oNav.style.top= (e.clientY<(aBody.clientHeight-160)? e.clientY: e.clientY-159)+"px";
+        return false;
     };
 })();
